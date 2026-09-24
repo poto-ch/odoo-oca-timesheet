716e31ed4fad62bc79136684309d301fff2bdeb9
Didier 'OdyX' Raboud <odyx@poto.ch>
2026-09-24 16:59:56 +0200

    [MIG] hr_timesheet_report_rounded: Migration to 18.0

--- hr_timesheet_report_rounded/__manifest__.py
+++ hr_timesheet_report_rounded/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Timesheet Report Rounded",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Human Resources",
     "summary": "Show rounded time in the Timesheet Reports",
     "website": "https://github.com/OCA/timesheet",
